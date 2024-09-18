917f41d29179165564052d906cdb3e1b804626ab
jam-jam200 <jessieoyaks4j@gmail.com>
2024-09-18 15:25:52 +0100

    student portal

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,13 +1,15 @@
 describe("QASTUDENT Application", () => {
-  it("passes", () => {
+  beforeEach(() => {
     cy.visit("https://qastudent-development.ogtlprojects.com");
   });
-});
 
-it("should navigate through Student Management and Course Management", () => {
-  cy.get('a[href="/students"]').click();
-  cy.url().should("include", "/students");
+  it("should navigate through Student and Course Management", () => {
+    cy.get('a[href="/students"]').should("be.visible").click();
+    cy.url().should("include", "/students");
 
-  cy.get('a[href="/courses"]').click();
-  cy.url().should("include", "/courses");
+    // Ensure the "Course Management" link is visible and click it
+    cy.get('a[href="/courses"]').should("be.visible").click();
+    // Check if the URL has changed to include "/courses"
+    cy.url().should("include", "/courses");
+  });
 });
